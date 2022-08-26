acca59ff41cce2ecfb89cab61ae2338f0cc15378
Isaiah Keller <isaiah.keller@gmail.com>
2022-08-25 22:04:36 -0700

    can retrieve weather data from front end search

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 // Require:
 const express = require('express');
 require('dotenv').config();
-let weatherData = require('./data/weather.json');
 const cors = require('cors');
 const axios = require('axios');
 
@@ -13,53 +12,42 @@ const PORT = process.env.REACT_APP_PORT || 5050;
 app.use(cors());
 app.listen(PORT, () => console.log(`It's Alive!! Listening on port: ${PORT}`));
 
+class Forecast {
+    constructor(obj) {
+        this.date = obj.valid_date;
+        this.description = obj.weather.description
+        this.temp = obj.temp
+    }
+}
+
 // Endpoints
 app.get('/', (request, response) => {
     console.log('Success!!');
     response.status(200).send('Home route')
 })
 
-class Forecast {
-    constructor(date, description) {
-        this.date = date;
-        this.description = description;
-    }
-}
-
 app.get('/weather', getWeather);
 
 async function getWeather(request, response) {
-
-    // process.env.REACT_APP_WEATHER_API_KEY
-
     let search = request.query.search;
     let lat = request.query.lat;
     let lon = request.query.lon;
     let cityData = {}
-    let weatherUrl = ``;
-    weatherData.forEach((element) => {
-        if( lat == element.lat && lon == element.lon ){
-            cityData = element
-        } 
-    })
-    const dayData = (num) => {
-        let idxNum = num - 1;
-        console.log(`cityData.data[idxNum] = ${cityData.data[idxNum]}`)
-        let date = cityData.data[idxNum].valid_date;
-        let desc = cityData.data[idxNum].weather.description;
-        return new Forecast(date, desc);
+    let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHER_API_KEY}`;
+
+    try {
+        let weatherResponse = await axios.get(weatherUrl);
+        let weatherArray = weatherResponse.data.data.map((element) => new Forecast(element))
+        console.log(weatherArray)
+        response.status(200).send(weatherArray)
+    } catch (err) {
+        console.log(`Error message: ${err}`)
+        response.status(500).send('server error')
     }
-    let one = dayData(1);
-    let two = dayData(2);
-    let three = dayData(3);
-    let forecastArray = [];
-    forecastArray.push(one, two, three);
-    console.log(forecastArray)
-    response.status(200).send(forecastArray);
 }
 
 app.get('/movies', async (request, response) => {
-    
+
 })
 
 // Catch all
