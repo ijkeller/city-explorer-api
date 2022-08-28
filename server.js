5b8fbd7a161da8cda2276d940d36cbd26d4e8f5c
Isaiah Keller <isaiah.keller@gmail.com>
2022-08-28 11:48:17 -0700

    fixed weather data

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ class Forecast {
         this.description = obj.weather.description;
         this.high_temp = obj.high_temp;
         this.low_temp = obj.low_temp;
-        this.precip = `${obj.precip * 100}%`
+        this.precip = `${
+            Math.round(obj.precip)
+        }%`
     }
 }
 
@@ -45,7 +47,7 @@ async function getWeather(request, response) {
     let lat = request.query.lat;
     let lon = request.query.lon;
     let cityData = {}
-    let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHER_API_KEY}`;
+    let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?units=I&lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHER_API_KEY}`;
 
     try {
         let weatherResponse = await axios.get(weatherUrl);
