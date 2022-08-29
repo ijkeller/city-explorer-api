26ba6a4ed7dfdb01cea2a57805b89f93d85252b5
Isaiah Keller <isaiah.keller@gmail.com>
2022-08-28 19:12:53 -0700

    added global cache and cache handling to both movies and weather, the initial retrieval and save to cache both work, the cache is displayed when console.logged in the if statement, but the localhost stays in a loading mode without displaying data.

--- modules/movies.js
+++ modules/movies.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let cache = require('./cache.js');
 const axios = require('axios');
 
 class MovieElement {
@@ -15,13 +16,23 @@ class MovieElement {
 async function getMovies(request, response) {
     let mdbKey = process.env.MOVIEDB_API_KEY;
     let query = request.query.search;
+    let key = `movies-${query}`
     let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${mdbKey}&page=1&query=${query}&include_adult=false`
 
     try {
-        let movieResponse = await axios.get(movieUrl);
-        let movieArray = movieResponse.data.results.map((element) => new MovieElement(element) )
-        response.status(200).send(movieArray)
-
+        if (cache[key] && (Date.now() - cache[key].timestamp < 2.16e+7)) {
+            console.log('Cache hit');
+            console.log(cache)
+            return cache[key].data;
+        } else {
+            console.log('Cache miss');
+            cache[key] = {};
+            cache[key].timestamp = Date.now();
+            let movieResponse = await axios.get(movieUrl);
+            let movieArray = movieResponse.data.results.map((element) => new MovieElement(element))
+            cache[key].data = movieArray
+            response.status(200).send(movieArray)
+        }
     } catch (err) {
         console.log(`Error message: ${err}`)
         response.status(500).send('server error')
